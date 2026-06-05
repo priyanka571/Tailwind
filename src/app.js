44c19e68dd4a8760518688fcefd263866ee0bcad
Priyanka Pandit <priyankapandit0012gmail.com>
2026-06-05 17:55:39 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-function hideAllSections(){
-    document.getElementById("addForm").classList.add("hidden");
-    document.getElementById("productList").classList.add("hidden");
-    document.getElementById("deleteSection").classList.add("hidden");
-
-}
 let openRequest = indexedDB.open("Products",1);
 console.log(openRequest);
 let db;
@@ -35,10 +29,14 @@ openRequest.onerror = (e) =>{
 
 
  function addProduct(){
-        hideAllSections();
-        document.getElementById("addForm").classList.remove("hidden");
+        event.preventDefault(); //to stop refresh
         const name= document.getElementById('name').value;
         const price = document.getElementById('price').value;
+        if (name.trim() === "" || price ===""){
+                    alert("please fill all field");
+                    return;
+        }
+
         let transaction = db.transaction("Products","readwrite")
          storeObject = transaction.objectStore("Products");
         let request = storeObject.add({
@@ -46,15 +44,14 @@ openRequest.onerror = (e) =>{
            name : name,
            price : Number(price)
         });
-        if (name.trim() === "" || price ===""){
-            alert("please fill all field");
-            return;
-        }
+       
        
         request.onsuccess = () => {
              alert("Item added successfully");
-             document.getElementById('name').value="";
-             document.getElementById('price').value="";
+             document.getElementById('name').value="";//input field empty
+             document.getElementById('price').value="";//input field empty
+             getProduct();
+             hideAddForm();
 
           console.log("Added");
         };
@@ -65,26 +62,36 @@ openRequest.onerror = (e) =>{
 }
      
 function getProduct(){
-    hideAllSections();
     document.getElementById("productList").classList.remove("hidden");
     let transaction = db.transaction("Products","readwrite")
     storeObject = transaction.objectStore("Products");
     let request = storeObject.getAll();
 
     request.onsuccess = (e) => {
-        // console.log(e.target.result);
+        
         let products = e.target.result;
+        if (products.length === 0) {
+                document.getElementById("productTableBody").innerHTML = `
+                    <tr>
+                                <td colspan="4" class="border p-4 text-center text-red-500">
+                            Inventory is empty
+                        </td>
+                    </tr>
+                `;
+                return;
+            }        
+
         let output = "";
         products.forEach((product) => {
             output += `
-            <tr>
-                <td class="border p-2">${product.id}</td>
-                <td class="border p-2">${product.name}</td>
-                <td class="border p-2">₹${product.price}</td>
+            <tr class="bg-white border-b hover:bg-gray-200 transition">
+                <td class="p-3 text-center text-gray-700 font-medium"">${product.id}</td>
+                <td class="p-3 text-center text-gray-800">${product.name}</td>
+                <td class="p-3 text-center text-green-600 font-semibold">₹${product.price}</td>
                  <td class="border p-2 text-center">
                     <button
                         onclick="delProduct(${product.id})"
-                        class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded">
+                        class="bg-[#3B7597] hover:bg-[#2C5EAD] text-white px-3 py-1 rounded">
                         Delete
                     </button>
                 </td>
@@ -101,27 +108,13 @@ function getProduct(){
     }
 } 
 function delProduct(id){
-    // hideAllSections();
-    // document.getElementById("deleteSection").classList.remove("hidden");
-
-    // const produuctName = document.getElementById('name').value;
     let transaction = db.transaction("Products","readwrite")
     storeObject = transaction.objectStore("Products");
-    // let nameIndex = storeObject.index("name");
     let request = storeObject.delete(id);
 
     request.onsuccess = (e) => {
-        // let product = e.target.result;
-        //  if (!product) {
-        //     console.log("Product not found");
-        //     return;
-        // }
         alert("Product deleted successfully");
         getProduct(); 
-        // let delRequest = storeObject.delete(product.id);
-        // delRequest.onsuccess = () => {
-        //     console.log("Deleted");
-        // };
     }; 
     request.onerror = (e) => {
         console.log(e.target.error);
@@ -130,12 +123,23 @@ function delProduct(id){
     
 }
 function manage(){
-                const menu = document.getElementById("menu");
-                menu.classList.toggle("hidden");
-            }
+                document.getElementById("homeSection").classList.add("hidden");
+                document.getElementById("manageSection").classList.remove("hidden");
+                getProduct();
+                
+}
+
+function showMenu(){
+    document.getElementById("menu").classList.toggle("hidden");
+}
+
 function showAddForm(){
                 document.getElementById("addForm")
                 .classList.remove("hidden");
 
+}
+
+function hideAddForm(){
+    document.getElementById("addForm").classList.add("hidden");
 }
        
